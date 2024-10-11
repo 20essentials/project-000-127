def91bf44df1d65e3a3ac82f1908aae1689f2f73
20essentials <s0l0estudi4r@gmail.com>
2024-10-11 20:34:59 +0200

    v0.0.1

---
+++ reference.md
@@ -0,0 +1,2 @@
+Master Manzdev: https://youtu.be/avMntSu5GAE?si=3bU5_1bZZnxNb_Wd
+Pattern Texture: https://www.vectorstock.com/royalty-free-vector/ufo-seamless-pattern-purple-aliens-on-yellow-vector-34495065
---
+++ script.js
@@ -0,0 +1,4 @@
+console.groupCollapsed('Reference ⬇️');
+console.log('Master Manzdev: https://youtu.be/avMntSu5GAE?si=3bU5_1bZZnxNb_Wd');
+console.log("Pattern", "https://www.vectorstock.com/royalty-free-vector/ufo-seamless-pattern-purple-aliens-on-yellow-vector-34495065")
+console.groupEnd();
